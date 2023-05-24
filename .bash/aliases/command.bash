7f64d210b7054e3dc6a969c5b5cbf6ba2fc17d17
scainimatteo <matteo.scaini.02031999@gmail.com>
2023-05-24 16:28:07 +0200

    Moved .rangerdir in /tmp folder

--- .bash/aliases/command.bash
+++ .bash/aliases/command.bash
@@ -2,7 +2,8 @@
 alias ip="py ~/.filerep/.scripts/ip.py; sob"
 alias PV='nvim ~/.private_variables'
 alias py='python3'
-alias ran='ranger --choosedir=$HOME/.terminal-config/.rangerdir; LASTDIR=`cat $HOME/.terminal-config/.rangerdir`; cd "$LASTDIR"'
+# alias ran='ranger --choosedir=$HOME/.terminal-config/.rangerdir; LASTDIR=`cat $HOME/.terminal-config/.rangerdir`; cd "$LASTDIR"'
+alias ran='ranger --choosedir=/tmp/.rangerdir; LASTDIR=`cat /tmp/.rangerdir`; cd "$LASTDIR"'
 alias list='pip3 list --format columns'
 alias randr='xrandr --output HDMI-1 --mode 2560x1440; conky'
 alias mcrandr='xrandr --output HDMI-1 --mode 1280x720'
